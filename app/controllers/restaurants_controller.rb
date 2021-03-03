a3996780c1a9af494e418cb0570d6378fa2bbb86
Miklos Ferencz <miklos.ferencz@gmail.com>
2021-03-03 21:48:17 +0100

    so far

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,35 +5,28 @@ class RestaurantsController < ApplicationController
     @restaurants = Restaurant.all
   end
   # A visitor can add a new restaurant, and be redirected to the show view of that new restaurant.
+  def show
+    @restaurant = Restaurant.find(params[:id])
+    @review = Review.new
+  end
+
   def new
     @restaurant = Restaurant.new
   end
 
-  def show
-    @restaurant = Restaurant.find(params[:id])
-  end
-
   def create
     @restaurant = Restaurant.new(restaurant_params)
-      if @restaurant.save
-      redirect_to @restaurant, notice: 'Restaurant was successfully created.'
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
     else
-      render :new
+      render "new"
     end
   end
 
-  def update
-  @restaurant = Restaurant.find(params[:id])
-    if @restaurant.update(restaurant_params)
-      redirect_to(@restaurant)
-    else
-      render "edit"
-    end
-  end
 
   private
 
   def restaurant_params
-    params.require(:restaurant).permit(:name, :address, :stars)
+    params.require(:restaurant).permit(:name, :phone_number, :category)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,9 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 Restaurant.destroy_all
 
-p bristol = Restaurant.new(name: "Epicure", category: "french")
-p london = Restaurant.new(name: "Red Lion", category: "english")
-p milano = Restaurant.new(name: "Pizza", category: "italian")
-p hamburg = Restaurant.new(name: "Hamburger", category: "german")
-p paris = Restaurant.new(name: "Resto", category: "french")
-
+bristol = Restaurant.new(name: "Epicure", category: "french")
+london = Restaurant.new(name: "Red Lion", category: "english")
+milano = Restaurant.new(name: "Pizza", category: "italian")
+hamburg = Restaurant.new(name: "Hamburger", category: "german")
+paris = Restaurant.new(name: "Resto", category: "french")
 
